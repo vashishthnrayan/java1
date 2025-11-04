a2ff43fac5902a646ad42688fe7ddc0da38a56f5
vashishthnrayan <vedmedicare123@gmail.com>
2025-11-04 17:05:59 +0530

    maggie

---
+++ maggie.java
@@ -0,0 +1,14 @@
+public class maggie {
+    public static void main(String[] args) {
+        System.out.println("My maggie recipe");
+        Maggie();
+    }
+        public static void Maggie() {
+            System.out.println("1. Boil water");
+            System.out.println("2. Add maggie masala");
+            System.out.println("3. Add noodles");
+            System.out.println("4. Cook for 2 minutes");
+            System.out.println("5. Serve hot");
+        }
+    
+}
