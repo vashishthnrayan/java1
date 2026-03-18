08b116a5e8eb33a200d2b77c6018434d235456c4
vashishthnrayan <vedmedicare123@gmail.com>
2026-03-18 12:20:45 +0530

    SelectionSort

---
+++ SelectionSort.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class SelectionSort {
+    void sort(int arr[]) {
+        int n = arr.length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    
+    void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int n = scn.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; ++i) {
+            arr[i] = scn.nextInt();
+        }
+
+        SelectionSort ob = new SelectionSort();
+        ob.sort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
+}
